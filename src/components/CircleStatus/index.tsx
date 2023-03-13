54bc2d87387d59348c2858b6130a6f35294fafd4
sichen <sichen@deeproute.ai>
2023-03-13 12:00:49 +0800

    feat: add circle status component

---
+++ src/components/CircleStatus/index.tsx
@@ -0,0 +1,41 @@
+import {
+  EXPERIMENTS_STATUS_LABELS,
+  TaskStatusEnum,
+  TaskTypeEnum,
+} from "@/types/enums";
+import styles from "./index.module.scss";
+import classNames from "classnames";
+
+export const CircleStatus = (props: {
+  status?: TaskStatusEnum;
+  type?: TaskTypeEnum;
+  defaultStatus?: string;
+}) => {
+  const { status, type, defaultStatus } = props;
+  const st = status ? status : type ? type : "";
+
+  return (
+    <div className={styles.counterContainer}>
+      <div className={classNames(styles.circle, styles[st])}>
+        {status && (
+          <i
+            className={classNames(
+              "icon sm-md white",
+              defaultStatus,
+              `i-${status}`
+            )}
+          ></i>
+        )}
+        {type && (
+          <i
+            className={classNames(
+              "al-icon lg al-color white",
+              `al-ico-type-${type.replace("_", "-")}`
+            )}
+          ></i>
+        )}
+      </div>
+      <div className={styles.counterLabel}>{EXPERIMENTS_STATUS_LABELS[st]}</div>
+    </div>
+  );
+};
