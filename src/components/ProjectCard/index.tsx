c82601738b75abfffe35c7908a2d745e12c39b94
sichen <sichen@deeproute.ai>
2023-03-13 11:59:45 +0800

    feat: project card simplify by card com

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -7,6 +7,7 @@ import { Button, Input, Space, Tooltip } from "antd";
 import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
 import { CircleCounter } from "@/components/CircleCounter";
 import { CircleTypeEnum } from "@/types/enums";
+import { DkCard } from "@/components/DkCard";
 
 export const ProjectCard = (props: {
   project?: Project;
@@ -14,9 +15,6 @@ export const ProjectCard = (props: {
   showAdd?: boolean;
 }) => {
   const { project, showMenu, showAdd } = props;
-  const [mainHovered, setMainHovered] = useState(false);
-  const [secHovered, setSecHovered] = useState(false);
-  const [showSec, setShowSec] = useState(false);
   const [showRename, setShowRename] = useState(false);
 
   function convertSecToDaysHrsMinsSec(secs: number) {
@@ -69,230 +67,93 @@ export const ProjectCard = (props: {
 
   return (
     <div className={styles.projectCard}>
-      <div className={styles.folderTab}>
-        <div className={styles.firstTab} onClick={() => setShowSec(false)}>
-          {!showSec ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="20"
-              viewBox="0 0 64 20"
-            >
-              {project?.sub_projects?.length && secHovered && (
-                <path
-                  d="M30.06,0H64V20H30.06C29.93,19.78,30.06,11.93,30.06,0Z"
-                  fill="#5a658e"
-                />
-              )}
-              {project?.sub_projects?.length && !secHovered && (
-                <path
-                  d="M30.06,0H64V20H30.06C29.93,19.78,30.06,11.93,30.06,0Z"
-                  fill="#2c3246"
-                />
-              )}
-              {mainHovered ? (
-                <path
-                  d="M35.61,0H6A6,6,0,0,0,0,6V20H60C44.82,12.36,49.37,0,35.61,0Z"
-                  fill="#5a658e"
-                  onMouseEnter={() => setMainHovered(true)}
-                  onMouseLeave={() => setMainHovered(false)}
-                />
-              ) : (
-                <path
-                  d="M35.61,0H6A6,6,0,0,0,0,6V20H60C44.82,12.36,49.37,0,35.61,0Z"
-                  fill="#384161"
-                  onMouseEnter={() => setMainHovered(true)}
-                  onMouseLeave={() => setMainHovered(false)}
-                />
-              )}
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="64"
-              height="20"
-              viewBox="0 0 64 20"
-            >
-              {mainHovered ? (
-                <path
-                  d="M35.61,0H6A6,6,0,0,0,0,6V20H60C44.82,12.36,49.37,0,35.61,0Z"
-                  fill="#5a658e"
-                  onMouseEnter={() => setMainHovered(true)}
-                  onMouseLeave={() => setMainHovered(false)}
-                />
-              ) : (
-                <path
-                  d="M35.61,0H6A6,6,0,0,0,0,6V20H60C44.82,12.36,49.37,0,35.61,0Z"
-                  fill="#384161"
-                  onMouseEnter={() => setMainHovered(true)}
-                  onMouseLeave={() => setMainHovered(false)}
-                />
-              )}
-              <path
-                d="M64,0V20H30C45.18,12.36,40.63,0,54.39,0Z"
-                fill="#2c3246"
-              />
-            </svg>
-          )}
-        </div>
-        {!!project?.sub_projects?.length && (
+      <DkCard
+        showFolder={true}
+        oneTabMode={!project?.sub_projects?.length}
+        showAdd={showAdd}
+        cardHeader={
           <>
-            <div
-              className={classNames(styles.middleTab, {
-                [styles.tabHovered]: secHovered,
-                [styles.showSecTab]: showSec,
-                [styles.secTabActive]: showSec,
-              })}
-              onClick={() => setShowSec(true)}
-              onMouseEnter={() => setSecHovered(true)}
-              onMouseLeave={() => setSecHovered(false)}
-            >
-              {`${project?.sub_projects?.length} sub projects`}
-            </div>
-            <div
-              className={classNames(styles.secondTab, {
-                [styles.tabHovered]: secHovered,
-                [styles.showSecTab]: showSec,
-              })}
-              onClick={() => setShowSec(true)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="20"
-                viewBox="0 0 28 20"
-              >
-                {(showSec || !secHovered) && (
-                  <path
-                    d="M3.61,0C17.37,0,12.82,12.36,28,20H0V0Z"
-                    fill="#2c3246"
-                  />
+            {project && (
+              <div className={styles.cardName}>
+                <Tooltip title={project.name} placement="bottom" color={"blue"}>
+                  <span className={styles.projectName}>
+                    {shortProjectName(project.name)}
+                  </span>
+                </Tooltip>
+                {showRename && (
+                  <Space>
+                    <Input />
+                    <Button type="text" icon={<CheckOutlined />} />
+                    <Button type="text" icon={<CloseOutlined />} />
+                  </Space>
                 )}
-                {!showSec && secHovered && (
-                  <path
-                    d="M3.61,0C17.37,0,12.82,12.36,28,20H0V0Z"
-                    fill="#5a658e"
-                  />
-                )}
-              </svg>
-            </div>
-          </>
-        )}
-      </div>
-      <div className={styles.cardContainer}>
-        {project && (
-          <>
-            {!showSec ? (
-              <div
-                className={classNames(styles.firstTabContainer, {
-                  [styles.firstTabHover]: mainHovered,
-                })}
-                onMouseEnter={() => setMainHovered(true)}
-                onMouseLeave={() => setMainHovered(false)}
-              >
-                <header>
-                  <div className={styles.cardName}>
-                    <Tooltip
-                      title={project.name}
-                      placement="bottom"
-                      color={"blue"}
-                    >
-                      <span className={styles.projectName}>
-                        {shortProjectName(project.name)}
-                      </span>
-                    </Tooltip>
-                    {showRename && (
-                      <Space>
-                        <Input />
-                        <Button type="text" icon={<CheckOutlined />} />
-                        <Button type="text" icon={<CloseOutlined />} />
-                      </Space>
-                    )}
-                  </div>
-                </header>
-                <div className={styles.line}></div>
-                <div className={styles.cardBody}>
-                  <div className={styles.cardCounter}>
-                    <CircleCounter
-                      counter={project.stats?.active?.total_tasks ?? "0"}
-                      label={"TOTAL"}
-                      type={
-                        project.stats?.active?.total_tasks === 0
-                          ? CircleTypeEnum.empty
-                          : CircleTypeEnum.pending
-                      }
-                    />
-                    <CircleCounter
-                      counter={
-                        project.stats?.active?.status_count?.in_progress ?? "0"
-                      }
-                      label={"RUNNING"}
-                      type={
-                        project.stats?.active?.status_count?.in_progress === 0
-                          ? CircleTypeEnum.empty
-                          : CircleTypeEnum.running
-                      }
-                    />
-                    <CircleCounter
-                      counter={
-                        project.stats?.active?.completed_tasks_24h ?? "0"
-                      }
-                      label={"COMPLETED"}
-                      underLabel="(24 hrs)"
-                      type={
-                        project.stats?.active?.completed_tasks_24h === 0
-                          ? CircleTypeEnum.empty
-                          : CircleTypeEnum.completed
-                      }
-                    />
-                  </div>
-                </div>
-                <footer>
-                  {project?.stats && (
-                    <div className={styles.footerTitle}>
-                      COMPUTE TIME:{" "}
-                      {convertSecToDaysHrsMinsSec(
-                        project.stats?.active?.total_runtime ?? 0
-                      )}
-                    </div>
-                  )}
-                </footer>
-              </div>
-            ) : (
-              <div className={styles.secondTabContainer}>
-                <div className={styles.subProjectsList}>
-                  {project?.sub_projects?.map((p) => (
-                    <a
-                      key={p.id}
-                      style={{ display: "flex", alignItems: "center" }}
-                    >
-                      <Tooltip title={p.name} placement="bottom" color={"blue"}>
-                        {subProjectNameTrans(p.name)}
-                      </Tooltip>
-                    </a>
-                  ))}
-                </div>
               </div>
             )}
           </>
-        )}
-        {showAdd && (
-          <div
-            className={classNames(styles.plusCard, {
-              [styles.plusCardHover]: mainHovered,
-            })}
-            onMouseEnter={() => setMainHovered(true)}
-            onMouseLeave={() => setMainHovered(false)}
-          >
-            <div
-              className={classNames(
-                "al-icon al-ico-plus al-color",
-                styles.plusIcon
-              )}
-            />
+        }
+        cardBody={
+          <>
+            {project && (
+              <div className={styles.cardCounter}>
+                <CircleCounter
+                  counter={project.stats?.active?.total_tasks ?? "0"}
+                  label={"TOTAL"}
+                  type={
+                    project.stats?.active?.total_tasks === 0
+                      ? CircleTypeEnum.empty
+                      : CircleTypeEnum.pending
+                  }
+                />
+                <CircleCounter
+                  counter={
+                    project.stats?.active?.status_count?.in_progress ?? "0"
+                  }
+                  label={"RUNNING"}
+                  type={
+                    project.stats?.active?.status_count?.in_progress === 0
+                      ? CircleTypeEnum.empty
+                      : CircleTypeEnum.running
+                  }
+                />
+                <CircleCounter
+                  counter={project.stats?.active?.completed_tasks_24h ?? "0"}
+                  label={"COMPLETED"}
+                  underLabel="(24 hrs)"
+                  type={
+                    project.stats?.active?.completed_tasks_24h === 0
+                      ? CircleTypeEnum.empty
+                      : CircleTypeEnum.completed
+                  }
+                />
+              </div>
+            )}
+          </>
+        }
+        cardFooter={
+          <>
+            {project?.stats && (
+              <div className={styles.footerTitle}>
+                COMPUTE TIME:{" "}
+                {convertSecToDaysHrsMinsSec(
+                  project.stats?.active?.total_runtime ?? 0
+                )}
+              </div>
+            )}
+          </>
+        }
+        subFolderTitle={`${project?.sub_projects?.length} sub projects`}
+        subCard={
+          <div className={styles.subProjectsList}>
+            {project?.sub_projects?.map((p) => (
+              <a key={p.id} style={{ display: "flex", alignItems: "center" }}>
+                <Tooltip title={p.name} placement="bottom" color={"blue"}>
+                  {subProjectNameTrans(p.name)}
+                </Tooltip>
+              </a>
+            ))}
           </div>
-        )}
-      </div>
+        }
+      />
     </div>
   );
 };
