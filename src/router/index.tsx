8780e29d59b6f719d22daf6f3f1fa5ab0e1323bf
sichen <sichen@deeproute.ai>
2023-03-28 18:34:49 +0800

    feat: remove nprogress component

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,8 +8,18 @@ import NotAuth from "@/components/errors/403"
 import NotFound from "@/components/errors/404"
 import NotNetwork from "@/components/errors/500"
 import { Overview } from "@/views/projects/overview"
+import progress from "nprogress"
+
+progress.configure({
+  easing: "ease", // 动画方式
+  speed: 500, // 递增进度条的速度
+  showSpinner: false, // 是否显示加载ico
+  trickleSpeed: 200, // 自动递增间隔
+  minimum: 0.3, // 初始化时的最小百分比
+})
 
 export const AuthRouter = (props: { children: JSX.Element }) => {
+  progress.start()
   const { children } = props
   const { pathname } = useLocation()
   if (pathname.includes("/share")) {
@@ -20,6 +30,7 @@ export const AuthRouter = (props: { children: JSX.Element }) => {
   if (!token) return <Navigate to="/login" replace />
   // todo add auth logic
   // * 当前账号有权限返回 Router，正常访问页面
+  progress.done()
   return children
 }
 
