903d31e1fcbfa0a91a98ce8aaccfce34231508c0
sichen <sichen@deeproute.ai>
2023-04-10 16:47:35 +0800

    feat: detail panel

--- src/views/projects/experiments/details/index.tsx
+++ src/views/projects/experiments/details/index.tsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom"
 import styles from "./index.module.scss"
 
-export const ExperimentDetails = () => {
+export const ExperimentDetails = (props: { children: JSX.Element }) => {
   const params = useParams()
+  const { children } = props
   return (
-    <div className={styles.experimentInfo}>Experiment:{params["expId"]}</div>
+    <div className={styles.experimentInfo}>
+      Experiment:{params["expId"]}
+      {children}
+    </div>
   )
 }
