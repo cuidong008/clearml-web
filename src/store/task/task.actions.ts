915266e3d3043c1d11efe7f356d3a20c9f3e3fec
sichen <sichen@deeproute.ai>
2023-04-13 19:46:55 +0800

    feat: detail header : name edit/id copy/menu/nav/comment edit/sync with list parent

--- src/store/task/task.actions.ts
+++ src/store/task/task.actions.ts
@@ -1,8 +1,17 @@
 import * as types from "./task.actions-types"
+import { Task } from "@/types/task"
 
 export const setTableColumn = (cols: string[]) => {
   return {
     type: types.TASK_SET_COLS,
     cols,
+  }
+}
+
+export const setSelectedTask = (selectedTask?: Task) => {
+  console.log("set t")
+  return {
+    type: types.TASK_SET_SELECTED,
+    selectedTask,
   }
 }
